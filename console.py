6cdd17d638dee8a983143ea9dee1f6778f731676
Mohamed Kaddouri <yeeloman@TheJester.eldergods.org>
2023-10-14 18:07:45 +0100

    add some files

--- console.py
+++ console.py
@@ -4,12 +4,18 @@
 import cmd
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
 from models import storage
 import json
 
 classes = {
     "BaseModel": BaseModel,
-    "User": User
+    "User": User,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity
 }
 
 
