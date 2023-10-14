3cef9ef2ddb18b0689339575e46710b0f71f5881
Mohamed Kaddouri <yeeloman@TheJester.eldergods.org>
2023-10-14 22:00:14 +0100

    tryna fix 7

--- console.py
+++ console.py
@@ -113,46 +113,77 @@ class HBNBCommand(cmd.Cmd):
             del obj_dic[dest_cls]
             storage.save()
 
-    def do_all(self, arg):
-        """Usage: all or all <class> or <class>.all()
-        Display string representations of all instances of a given class.
-        If no class is specified, displays all instantiated objects."""
-        argl = HBNBCommand.parseLine(arg)
-        if len(argl) > 0 and argl[0] not in classes.keys():
-            print("** class doesn't exist **")
-        else:
-            objl = []
-            for obj in storage.all().values():
-                if len(argl) > 0 and argl[0] == obj.__class__.__name__:
-                    objl.append(obj.__str__())
-                elif len(argl) == 0:
-                    objl.append(obj.__str__())
-            print(objl)
+    def do_all(self, line):
+        """do_all.
+            Prints all string representation of all\
+instances based or not on the class name
 
-    def do_update(self, line):
-        """Updates an instance based on the class name and id
-
-        Usage: Update <Class_name> <Class_id> <attribute> <value>
+        Usage: all <Class_name(optional)>
         """
-        token = HBNBCommand.parseLine(line)
+        all_dic = []
         obj_dic = storage.all()
-        if token == []:
-            print("** class name missing **")
-        elif token[0] not in classes.keys():
+        token = HBNBCommand.parseLine(line)
+        if len(token) > 0 and token[0] not in classes.keys():
             print("** class doesn't exist **")
-        elif len(token) == 1:
-            print("** instance id missing **")
-        elif f"{token[0]}.{token[1]}" not in obj_dic:
-            print("** no instance found **")
-        elif len(token) == 2:
-            print("** attribute name missing **")
-        elif len(token) == 3:
-            print("** value missing **")
+        elif len(token) > 0 and token[0] in classes.keys():
+            for obj in obj_dic.values():
+                if token[0] == obj.__class__.__name__:
+                    all_dic.append(obj.__str__())
+            print(all_dic)
         else:
-            update_dic = obj_dic["{}.{}".format(token[0], token[1])]
-            attrtype = type(update_dic.__dict__[token[2]])
-            setattr(update_dic, token[2], attrtype(token[3]))
-            storage.save()
+            for obj in obj_dic.values():
+                all_dic.append(obj.__str__())
+            print(all_dic)
+
+    def do_update(self, arg):
+        """Usage: update <class> <id> <attribute_name> <attribute_value> or
+       <class>.update(<id>, <attribute_name>, <attribute_value>) or
+       <class>.update(<id>, <dictionary>)
+        Update a class instance of a given id by adding or updating
+        a given attribute key/value pair or dictionary."""
+        argl = HBNBCommand.parseLine(arg)
+        objdict = storage.all()
+
+        if len(argl) == 0:
+            print("** class name missing **")
+            return False
+        if argl[0] not in classes.keys():
+            print("** class doesn't exist **")
+            return False
+        if len(argl) == 1:
+            print("** instance id missing **")
+            return False
+        if "{}.{}".format(argl[0], argl[1]) not in objdict.keys():
+            print("** no instance found **")
+            return False
+        if len(argl) == 2:
+            print("** attribute name missing **")
+            return False
+        if len(argl) == 3:
+            try:
+                type(eval(argl[2])) != dict
+            except NameError:
+                print("** value missing **")
+                return False
+
+        if len(argl) == 4:
+            obj = objdict["{}.{}".format(argl[0], argl[1])]
+            if argl[2] in obj.__class__.__dict__.keys():
+                valtype = type(obj.__class__.__dict__[argl[2]])
+                obj.__dict__[argl[2]] = valtype(argl[3])
+            else:
+                obj.__dict__[argl[2]] = argl[3]
+        elif type(eval(argl[2])) == dict:
+            obj = objdict["{}.{}".format(argl[0], argl[1])]
+            for k, v in eval(argl[2]).items():
+                if (k in obj.__class__.__dict__.keys() and
+                        type(obj.__class__.__dict__[k]) in {str, int, float}):
+                    valtype = type(obj.__class__.__dict__[k])
+                    obj.__dict__[k] = valtype(v)
+                else:
+                    obj.__dict__[k] = v
+        storage.save()
+        storage.save()
 
 
 if __name__ == '__main__':
