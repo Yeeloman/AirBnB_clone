9aa44dcab1c974af67ac1d014b86628d590a784e
Mohamed Kaddouri <yeeloman@TheJester.eldergods.org>
2023-10-14 17:43:09 +0100

    add user class

--- console.py
+++ console.py
@@ -3,11 +3,13 @@
 
 import cmd
 from models.base_model import BaseModel
+from models.user import User
 from models import storage
 import json
 
 classes = {
-    "BaseModel": BaseModel
+    "BaseModel": BaseModel,
+    "User": User
 }
 
 
